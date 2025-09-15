7fce2c757f837cd4c9afcf803c41d9a145ecaba0
gendraM <laurellemankassa@gmail.com>
2025-09-15 22:34:24 +0000

    ok defis +suppresion defis en cours next step referentiel alimentaire plus maj rapide

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -10,11 +10,6 @@ globalThis.__BUILD_MANIFEST = {
   "rootMainFiles": [],
   "rootMainFilesTree": {},
   "pages": {
-    "/": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/index.js"
-    ],
     "/_app": [
       "static/chunks/webpack.js",
       "static/chunks/main.js",
@@ -29,26 +24,6 @@ globalThis.__BUILD_MANIFEST = {
       "static/chunks/webpack.js",
       "static/chunks/main.js",
       "static/chunks/pages/defis.js"
-    ],
-    "/plan": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/plan.js"
-    ],
-    "/profil": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/profil.js"
-    ],
-    "/suivi": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/suivi.js"
-    ],
-    "/suivi-poids": [
-      "static/chunks/webpack.js",
-      "static/chunks/main.js",
-      "static/chunks/pages/suivi-poids.js"
     ],
     "/tableau-de-bord": [
       "static/chunks/webpack.js",
